ed032177d4c4a4476c8a3e4012a908db692b92f4
dejiayodejii <dejiayodeji010@gmail.com>
2024-02-21 19:43:59 +0100

    google sign added

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,36 @@
 const UserModel = require("../models/user_model");
 const Utils = require("../utils/generateToken");
-const bcryptjs = require("bcryptjs");
 const validateRegistrationInput = require("../validators/reg_validator");
 const validateLoginInput = require('../validators/login_validator');
 const Helpers = require('./helpers');
 
 const Auth = {};
+
+
+Auth.googleSignIn = async (req, res) => {
+  const { id, email, name } = req.body;
+  let user = await UserModel.findOne({ email });
+
+  if (!user) {
+    const otp = Utils.generateOTP();
+
+    await new UserModel({
+      name,
+      email,
+      googleID: id,
+      source: "Google",
+      verificationCode: otp,
+    }).save();
+
+    Utils.sendOTP(email, otp);
+
+    return res.status(200).json({ message: "User registered successfully!, An otp has been sent to your email to verify your account", });
+
+  }
+  //generate jwt token
+  return res.status(200).json({ message: 'Login Succesful' });
+
+}
 
 Auth.signUp = async (req, res) => {
   const { error } = await validateRegistrationInput(req.body);
@@ -15,7 +40,7 @@ Auth.signUp = async (req, res) => {
       error: error.details[0].message,
     });
   }
-  const { email, password, phoneNumber, name, source } = req.body;
+  const { email, password, phoneNumber, name } = req.body;
 
   try {
     const existingUser = await UserModel.findOne({
@@ -33,7 +58,7 @@ Auth.signUp = async (req, res) => {
       phone: phoneNumber,
       email: email,
       password: Helpers.HashValue(password),
-      source: source,
+      source: 'Email',
       verificationCode: otp,
     }).save();
 
@@ -53,24 +78,39 @@ Auth.signUp = async (req, res) => {
 
 Auth.login = async (req, res) => {
 
-  const { email, password } = req.body;
-  const { errors, isValid } = validateLoginInput({ email, password });
+  try {
+    const { email, password } = req.body;
+    const { errors, isValid } = validateLoginInput({ email, password });
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const user = UserModel.findOne({ email: email.toLowerCase().trim() });
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const user = UserModel.findOne({ email: email.toLowerCase().trim() });
 
-  if (!user) {
-    errors.email = "No account matched with email provided";
-    return res.status(404).json(errors);
+    if (!user) {
+      errors.email = "No account matched with email provided";
+      return res.status(404).json(errors);
+    }
+    if (user.source === 'Google') {
+      errors.password =
+        "Please use the 'Log in with Google' button to access your account";
+      return res.status(401).json(errors);
+    }
+    let isPassword = Helpers.UnHashValue(password, user.password);
+    if (isPassword) {
+      //generate jwt token next
+      return res.status(200).json({ message: 'Login Succesful' });
+    } else {
+      errors.password = "Password is incorrect";
+      return res.status(401).json(errors);
+    }
+
+  } catch (error) {
+    console.error('Error in login:', error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
-  let isPassword = Helpers.UnHashValue(password, user.password);
-  if (isPassword) {
-    return res.status(200).json({ message: 'Login Succesful' });
-  }
-  errors.password = "Password is incorrect";
-  return res.status(401).json(errors);
+
+
 
 };
 
